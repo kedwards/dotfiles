0c9d5b6231a165330842d992c43734d7bdf0d715
Kevin Edwards <kedwards@kevinedwards.ca>
2026-01-12 07:07:53 -0700

    Fix screenshot function to use '-zo' option

--- home/dot_local/bin/i3ctl.d/lock.sh
+++ home/dot_local/bin/i3ctl.d/lock.sh
@@ -6,7 +6,7 @@ lock_cmd() {
   require magick
 
   if command -v scrot >/dev/null 2>&1; then
-    screenshot() { scrot -z "$1"; }
+    screenshot() { scrot -zo "$1"; }
   elif command -v maim >/dev/null 2>&1; then
     screenshot() { maim "$1"; }
   else
@@ -15,7 +15,7 @@ lock_cmd() {
 
   local img
   img="$(mktemp "${XDG_RUNTIME_DIR:-/tmp}/i3lock.XXXXXX.png")"
-  trap 'rm -f "$img"' EXIT INT TERM
+  trap "rm -f '$img'" EXIT INT TERM
 
   screenshot "$img"
   magick "$img" -blur 5x4 "$img"
